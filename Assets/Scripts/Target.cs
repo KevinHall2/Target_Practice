8410c096326b8a21ebaf50fbfcb35e4a82f94df6
KevinHall2 <khjiitech@gmail.com>
2025-04-23 17:03:10 -0500

    Third initialization commit that includes player camera functions, target spawning functions, and a crosshair

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    //Creates the illusion of the target being destroyed by moving it to a different random position
     public void Hit()
     {
         transform.position = TargetBounds.Instance.GetRandomPosiiton();
